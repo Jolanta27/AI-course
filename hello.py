b1c455fbd7b4e64713d84340e6ff65c15af66989
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-03-25 16:26:56 +0100

    some exercises of loops in python

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello world")
+name = input("What's your name?")
+print("hello, " + name)
+print("hello, \"friend\"")
+print(f"hello, {name}")
+
+name = name.strip()
+#remove whitespace from str from the left and right side
+name = name.capitalize()
+# capitalize just the first letter of str only from the first letter!!!
+name = name.title() 
+#make first letter capitalized of every word not just the first one
+name_Two = "Johan"
+
+print(name_Two.split(" "))
+print(int(-2))
+
+
